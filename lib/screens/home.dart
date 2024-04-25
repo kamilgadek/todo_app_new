133e356c7288a18aec6eeef36d4c07b15fabe43e
Kamil Gadek <kamil.gadek124@gmail.com>
2024-04-25 19:15:41 +0200

    Create search box

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -14,8 +14,50 @@ class Home extends StatelessWidget {
               Icons.menu,
               size: 30,
             ),
-            CircleAvatar(backgroundImage: AssetImage('images/foto1.jpg'),)
+            CircleAvatar(
+              backgroundImage: AssetImage('images/foto1.jpg'),
+            )
           ],
+        ),
+      ),
+      body: Container(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 15,
+        ),
+        child: const Column(
+          children: [
+            SizedBox(height: 10,),
+            _SearchBox(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _SearchBox extends StatelessWidget {
+  const _SearchBox();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        color: Colors.grey[100],
+      ),
+      child: const TextField(
+        decoration: InputDecoration(
+          prefixIcon: Icon(
+            Icons.search,
+            size: 20,
+          ),
+          prefixIconConstraints: BoxConstraints(
+            maxHeight: 20,
+            minWidth: 25,
+          ),
+          border: InputBorder.none,
+          hintText: 'Search'
         ),
       ),
     );
